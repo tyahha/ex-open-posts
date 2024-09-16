2f73297f34a82c8f90b77f02c7901df989641bfb
toshinori.sato <tyahhahha@gmail.com>
2024-09-16 19:46:43 +0900

    :sparkles: usersドキュメントを作成できる

--- app/lib/firebase/firebaseRulesTests/users.test.ts
+++ app/lib/firebase/firebaseRulesTests/users.test.ts
@@ -23,19 +23,34 @@ beforeEach(async () => {
 
 describe("users rules", () => {
   describe("create valid with valid data", () => {
+    const validData = {
+      name: "dummy-name",
+      birthDay: "1992-03-05",
+      gender: GENDER.MALE,
+    } satisfies RawUser;
+
     describe("un authenticated", () => {
       const subject = async () => {
         const firestore = testEnv.unauthenticatedContext().firestore();
         const docRef = await doc(firestore, `/users/dummy-user-id`);
-        return setDoc(docRef, {
-          name: "dummy-name",
-          birthDay: "1992-03-05",
-          gender: GENDER.MALE,
-        } satisfies RawUser);
+        return setDoc(docRef, validData);
       };
 
       it("should fail to create", async () => {
-        await expect(assertFails(subject())).resolves.toBeTruthy();
+        await assertFails(subject());
+      });
+    });
+
+    describe("authenticated", () => {
+      const subject = async () => {
+        const userId = "valid-user-id";
+        const firestore = testEnv.authenticatedContext(userId).firestore();
+        const docRef = await doc(firestore, `/users/${userId}`);
+        return setDoc(docRef, validData);
+      };
+
+      it("should success to create", async () => {
+        await assertSucceeds(subject());
       });
     });
   });
