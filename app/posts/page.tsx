fa45cdb80b9b2a0659b31c36cc249cd207c027f3
toshinori.sato <tyahhahha@gmail.com>
2024-09-16 17:31:48 +0900

    :sparkles: ログイン後のヘッダを作成

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import styles from "./page.module.css";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
 export default function PostsPage() {
-  return <div>posts</div>;
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  useEffect(() => {
+    const handler = (e: Event) => {
+      if (
+        e.target instanceof HTMLElement &&
+        e.target.closest(`.${styles.profileMenuButton}`)
+      ) {
+        return;
+      }
+      setIsOpenMenu(false);
+    };
+    window.addEventListener("click", handler);
+
+    return () => {
+      window.removeEventListener("click", handler);
+    };
+  }, []);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.appName}>EX Open Poster</h1>
+        <button
+          className={styles.profileMenuButton}
+          onClick={() => setIsOpenMenu(true)}
+        >
+          <Image
+            src="/anonymous.png"
+            alt="ユーザーのプロフィール画像"
+            width={44}
+            height={44}
+          />
+        </button>
+        <div
+          className={clsx(styles.menuBody, {
+            [styles.visible]: isOpenMenu,
+          })}
+        >
+          <div className={styles.menuItem}>ログアウト</div>
+        </div>
+      </header>
+    </>
+  );
 }
