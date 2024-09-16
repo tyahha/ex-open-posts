21d5f2abfd7edad8f74fe06d73f3ef5ecb768d2c
toshinori.sato <tyahhahha@gmail.com>
2024-09-16 11:52:43 +0900

    :hammer: 共通で利用できそうなCSSを共通で参照しやすい場所へ配置

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import commonStyles from "./ui/common.module.css";
 import styles from "./page.module.css";
 import clsx from "clsx";
 import Link from "next/link";
@@ -5,16 +6,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.content}>
+      <div className={commonStyles.content}>
         <h1 className={styles.label}>EX Open Posts</h1>
         <p className={styles.description}>
           ユーザー同士がテキストを共有しあえるサービスです
         </p>
         <div className={styles.buttonBox}>
-          <Link href="/login" className={clsx(styles.button, styles.login)}>
+          <Link
+            href="/login"
+            className={clsx(commonStyles.button, commonStyles.login)}
+          >
             ログイン
           </Link>
-          <Link href="/sign-up" className={clsx(styles.button, styles.signUp)}>
+          <Link
+            href="/sign-up"
+            className={clsx(commonStyles.button, commonStyles.signUp)}
+          >
             新規登録
           </Link>
         </div>
