d677768eeb5e2052108ab3323d9e7234b55150e1
toshinori.sato <tyahhahha@gmail.com>
2024-09-17 00:02:28 +0900

    :sparkles: メール送信画面からログイン画面に遷移できる

--- app/auth/sent-email/page.tsx
+++ app/auth/sent-email/page.tsx
@@ -1,4 +1,6 @@
 import { MainContent } from "@/app/ui/MainContent";
+import commonStyles from "@/app/ui/common.module.css";
+import Link from "next/link";
 
 export default function SentEmailPage() {
   return (
@@ -6,6 +8,11 @@ export default function SentEmailPage() {
       メールアドレス確認のためのメールを送信しました。
       <br />
       メールを確認して登録を完了してください。
+      <div className={commonStyles.buttonBox}>
+        <Link href="/login" className={commonStyles.button}>
+          ログイン画面に進む
+        </Link>
+      </div>
     </MainContent>
   );
 }
